782c7dae99552545775a815b3dac854f4ef84578
Alan Sparrow <git@freeflight.org.uk>
2024-07-24 16:33:34 +0100

    USB serial receive working

--- app/src/main/java/uk/org/freeflight/canbellupload/MainActivity.kt
+++ app/src/main/java/uk/org/freeflight/canbellupload/MainActivity.kt
@@ -1,9 +1,12 @@
 package uk.org.freeflight.canbellupload
 
+import android.hardware.usb.UsbManager
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
@@ -19,28 +22,78 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.hoho.android.usbserial.driver.UsbSerialPort
+import com.hoho.android.usbserial.driver.UsbSerialProber
+import com.hoho.android.usbserial.util.SerialInputOutputManager
 import uk.org.freeflight.canbellupload.ui.theme.CanBellUploadTheme
 
-class MainActivity : ComponentActivity() {
+
+class MyViewModel: ViewModel() {
+    var txt by mutableStateOf("")
+
+    fun append(data: String) {
+        Log.d("FOO", data)
+        txt += data
+    }
+}
+
+class MainActivity : ComponentActivity(), SerialInputOutputManager.Listener {
+    private lateinit var usbIoManager: SerialInputOutputManager
+    private lateinit var usbSerialPort: UsbSerialPort
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
         setContent {
             CanBellUploadTheme {
-                CanBellScaffold()
+                CanBellScaffold { getData() }
             }
         }
+    }
+
+    override fun onNewData(bytes: ByteArray) {
+        runOnUiThread {receive(bytes)}
+    }
+
+    override fun onRunError(e: Exception) {
+        runOnUiThread {Log.d("FOO", "Run error")}
+    }
+
+    private fun receive(bytes: ByteArray) {
+        val myViewModel: MyViewModel by viewModels()
+        myViewModel.append(bytes.decodeToString())
+    }
+
+    private fun getData() {
+        val usbManager = getSystemService(USB_SERVICE) as UsbManager
+
+        val availableDrivers = UsbSerialProber.getDefaultProber().findAllDrivers(usbManager)
+        if (availableDrivers.isEmpty())
+            return
+
+        val driver = availableDrivers[0]
+        val connection = usbManager.openDevice(driver.device)
+
+        usbSerialPort = driver.ports[0]
+        usbSerialPort.open(connection)
+        usbSerialPort.dtr = true
+
+        usbIoManager = SerialInputOutputManager(usbSerialPort, this)
+        usbIoManager.start()
     }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun CanBellScaffold() {
-    var contents by rememberSaveable { mutableStateOf("Hello")}
+fun CanBellScaffold(onClick: () -> Unit) {
+    val myViewModel: MyViewModel = viewModel()
+    val contents = myViewModel.txt
 
     Scaffold(
         topBar = {
@@ -56,11 +109,9 @@ fun CanBellScaffold() {
         },
         bottomBar = {
             BottomAppBar {
-                Button(onClick = { contents = "Goodbye"}) {
-                    Text(
-                        text ="Hello",
-                        style = MaterialTheme.typography.bodyLarge,
-                    )
+                //Button(onClick = { contents = "Goodbye"}) {
+                Button(onClick = { onClick() }) {
+                    Text(text ="Hello", style = MaterialTheme.typography.bodyLarge)
                 }
             }
         }
@@ -79,6 +130,6 @@ fun CanBellScaffold() {
 @Composable
 fun ScaffoldPreview() {
     CanBellUploadTheme {
-        CanBellScaffold()
+        CanBellScaffold {}
     }
 }
